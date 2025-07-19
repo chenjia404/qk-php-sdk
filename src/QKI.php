b85b13988ab70dce0a92178abceee74b28f1ab35
chenjia404 <chenjiablog@gmail.com>
2025-07-19 17:01:46 +0800

    init

---
+++ src/QKI.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: woshi
+ * Date: 2018/12/12
+ * Time: 18:30
+ */
+namespace quarkblockchain;
+
+use EthereumRPC\BcMath;
+use EthereumRPC\EthereumRPC;
+use EthereumRPC\Exception\GethException;
+
+class QKI extends \EthereumRPC\API\Eth
+{
+}
---
+++ src/QkNodeRPC.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: woshi
+ * Date: 2018/12/12
+ * Time: 18:28
+ */
+namespace quarkblockchain;
+
+class QkNodeRPC extends \EthereumRPC\EthereumRPC
+{
+    /** @var QKI */
+    private $qki;
+
+    /**
+     * EthereumRPC constructor.
+     * @param string $host
+     * @param int|null $port
+     */
+    public function __construct(string $host, ?int $port = null, ?bool $ssl = true)
+    {
+        parent::__construct($host,$port,$ssl);
+        $this->qki = new QKI($this);
+    }
+
+    public function QKI()
+    {
+        return $this->qki;
+    }
+}
